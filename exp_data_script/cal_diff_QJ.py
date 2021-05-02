3bd7b9e88b2bcb38276e016f645da32fcd07e016
洪澤厚 <tsehou@hongzehous-MacBook-Pro.local>
2021-05-02 11:48:33 +0800

    modify optimal score calculation

--- exp_data_script/cal_diff_QJ.py
+++ exp_data_script/cal_diff_QJ.py
@@ -9,7 +9,7 @@ from utils import cal_OPT_performance
 
 
 def PTP_diff_QJ(requested_quality_in_CEL, istrain):
-    TMIV_csv_folder_path = "..\\db_preprocessing\\datasets_pers\\states\\train"
+    TMIV_csv_folder_path = "../db_preprocessing/datasets_pers/states/train"
     PTP_dataset_names = [
         "IntelFrog",
         "OrangeKitchen",
@@ -20,41 +20,47 @@ def PTP_diff_QJ(requested_quality_in_CEL, istrain):
         "TechnicolorPainter",
     ]
 
-    DEF_CEL = 0.0
-    OPT_CEL = 0.0
-    DRL_CEL = 0.0
-    CNN_CEL = 0.0
+    DEF_performances = []
+    OPT_performances = []
+    DRL_performances = []
+    CNN_performances = []
+    DEF_min = 0.0
+    OPT_min = 0.0
+    DRL_min = 0.0
+    CNN_min = 0.0
     for idx, dataset in enumerate(PTP_dataset_names):
         # DEF
         print(f"loading DEF")
         DEF_performance = cal_DEF_performance(
             "DEF_OPT_template_PTP.csv",
-            "..\\db_preprocessing\\datasets_pers\\states\\train",
+            "../db_preprocessing/datasets_pers/states/train",
             requested_quality_in_CEL,
         )
         if istrain == 1:
             DEF_performance = DEF_performance[DEF_performance["Dataset"] != dataset]
         else:
             DEF_performance = DEF_performance[DEF_performance["Dataset"] == dataset]
-        DEF_CEL += DEF_performance["CEL"].mean()
+        DEF_performances += DEF_performance
+        DEF_min = min(DEF_min, DEF_performance["CEL"].min())
         #
         # OPT
         print(f"loading OPT")
         OPT_performance = cal_OPT_performance(
             "DEF_OPT_template_PTP.csv",
-            "..\\db_preprocessing\\datasets_pers\\states\\train",
+            "../db_preprocessing/datasets_pers/states/train",
             requested_quality_in_CEL,
         )
         if istrain == 1:
             OPT_performance = OPT_performance[OPT_performance["Dataset"] != dataset]
         else:
             OPT_performance = OPT_performance[OPT_performance["Dataset"] == dataset]
-        OPT_CEL += OPT_performance["CEL"].mean()
+        OPT_performances += OPT_performance
+        OPT_min = min(OPT_min, OPT_performance["CEL"].min())
         #
         # DRL
         print(f"loading {dataset}, DRL")
         prediction_csv = (
-            f"..\\NN-based_algorithm_seleted_predict_result\\PTP\\{dataset}-6_obj_nsv_best.csv"
+            f"../NN-based_algorithm_seleted_predict_result/PTP/{dataset}-6_obj_nsv_best.csv"
         )
         DRL_performance = cal_prediction_performance(
             prediction_csv, TMIV_csv_folder_path, requested_quality_in_CEL
@@ -63,12 +69,14 @@ def PTP_diff_QJ(requested_quality_in_CEL, istrain):
             DRL_performance = DRL_performance[DRL_performance["Dataset"] != dataset]
         else:
             DRL_performance = DRL_performance[DRL_performance["Dataset"] == dataset]
-        DRL_CEL += DRL_performance["CEL"].mean()
+        DRL_performances += DRL_performance
+        DRL_min = min(DRL_min, DRL_performance["CEL"].min())
+
         #
         # CNN
         print(f"loading {dataset}, CNN")
         prediction_csv = (
-            f"..\\NN-based_algorithm_seleted_predict_result\\PTP\\{dataset}-6-cnn_obj_nsv.csv"
+            f"../NN-based_algorithm_seleted_predict_result/PTP/{dataset}-6-cnn_obj_nsv.csv"
         )
         CNN_performance = cal_prediction_performance(
             prediction_csv, TMIV_csv_folder_path, requested_quality_in_CEL
@@ -77,27 +85,35 @@ def PTP_diff_QJ(requested_quality_in_CEL, istrain):
             CNN_performance = CNN_performance[CNN_performance["Dataset"] != dataset]
         else:
             CNN_performance = CNN_performance[CNN_performance["Dataset"] == dataset]
-        CNN_CEL += CNN_performance["CEL"].mean()
+        CNN_performances += CNN_performance
+        CNN_min = min(CNN_min, CNN_performance["CEL"].min())
+
         #
-    DEF_CEL /= 7
-    OPT_CEL /= 7
-    DRL_CEL /= 7
-    CNN_CEL /= 7
-    sns.barplot(
-        x=["DEF", "CNN", "DRL"], y=[DEF_CEL / OPT_CEL, CNN_CEL / OPT_CEL, DRL_CEL / OPT_CEL]
-    )
-    print([DEF_CEL / OPT_CEL, CNN_CEL / OPT_CEL, DRL_CEL / OPT_CEL])
+    
+    gmin = min(DEF_min, OPT_min, CNN_min, DRL_min)
+    DEF_performances['optimal score'] = (DEF_performances['CEL'] - gmin) / (OPT_performances['CEL'] - gmin)
+    DEF_OS = DEF_performances['optimal score'].mean()
+    CNN_performances['optimal score'] = (CNN_performances['CEL'] - gmin) / (OPT_performances['CEL'] - gmin)
+    CNN_OS = CNN_performances['optimal score'].mean()
+    DRL_performances['optimal score'] = (DRL_performances['CEL'] - gmin) / (OPT_performances['CEL'] - gmin)
+    DRL_OS = DRL_performances['optimal score'].mean()
+    pd_OS = pd.DataFrame(data={"name":["DEF", "CNN", "DRL"],"OS":[DEF_OS, CNN_OS, DRL_OS]})
+    pd_OS.plot.bar(x="name",y="OS")
+    # sns.barplot(
+    #     x=["DEF", "CNN", "DRL"], y=[DEF_OS, CNN_OS, DRL_OS]
+    # )
+    print([DEF_OS, CNN_OS, DRL_OS])
 
 
 #%%
 PTP_diff_QJ(18, 0)
 #%%
-PTP_diff_QJ(20, 0)
-#%%
-PTP_diff_QJ(22, 0)
-#%%
-PTP_diff_QJ(24, 0)
-#%%
-PTP_diff_QJ(26, 0)
+# PTP_diff_QJ(20, 0)
+# #%%
+# PTP_diff_QJ(22, 0)
+# #%%
+# PTP_diff_QJ(24, 0)
+# #%%
+# PTP_diff_QJ(26, 0)
 
 # %%
